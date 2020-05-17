85b5c147a82eca271c429fd700f6371553683a30
dannieldev <danielcrespin.dev@gmail.com>
2020-05-17 08:31:28 -0600

    Deterner video en la misma pagina al no ser visualizado (IntersectionObserver)

--- mediaplayer/assets/plugins/AutoPause.js
+++ mediaplayer/assets/plugins/AutoPause.js
@@ -1,24 +1,28 @@
 class AutoPause {
   constructor() {
-    this.threshold = 0.25;
+    this.threshold = 0.25; //El porcentage de intersection
     this.handleIntersection = this.handleIntersection.bind(this);
     this.handleVisibilityChange = this.handleVisibilityChange.bind(this);
   }
 
-  run(player) {
+  run(player) { //Recibe el Player
     this.player = player;
+    //Observar la intercepsion 
     const observer = new IntersectionObserver(this.handleIntersection, {
+    //threshold: this.threshold: Que % del elemento tiene con todo el contenedor
       threshold: this.threshold,
     });
 
     //Pasar saber si el usuario esta visualizando la pagina 
-    observer.observe(this.player.media);
+    observer.observe(this.player.media);//Media es toda la pantalla
     document.addEventListener('visibilitychange', this.handleVisibilityChange);
   }
 
- handleIntersection(entries) {
-    const entry = entries[0];
+ handleIntersection(entries) {//entries son todos los objetos que estamos observando
+    const entry = entries[0]; //En este caso solo hay uno
+    console.log(entry)
 
+    //Determinar la intersection 
     const isVisible = entry.intersectionRatio >= this.threshold;
 
     if (isVisible) {
